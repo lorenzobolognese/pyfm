ffbb4d1d74295a65806af8f81a491a65ff943753
Lorenzo Bolognese <57444324+lorenzobolognese@users.noreply.github.com>
2019-12-09 17:19:46 +0100

    Restore fixed penalization

--- player.py
+++ player.py
@@ -9,8 +9,6 @@
 # Licence:     MIT
 #-------------------------------------------------------------------------------
 
-import random
-
 class Player(object):
     def __init__(self, rateA, rateM, rateD, rateGK, stamina, role, side, name = "John Doe"):
         self.role = role
@@ -28,11 +26,10 @@ class Player(object):
         self.ResetEnergy()
 
     def GetStats(self, fieldRole, fieldSide):
-        # When out of role of side, player is penalized by a random factor between 0,5 and 1
         if fieldRole in self.role: r = 100
-        else: r = random.randint(50,100)
+        else: r = 50
         if (fieldRole == "GK") or (fieldSide in self.side): p = 100
-        else: p = random.randint(50,100)
+        else: p = 50
         return int(self.rateA*(r/100)*(p/100)), int(self.rateM*(r/100)*(p/100)), int(self.rateD*(r/100)*(p/100)), int(self.rateGK*(r/100)*(p/100))
 
     def GetPerformance(self, zone, fieldRole, fieldSide):
