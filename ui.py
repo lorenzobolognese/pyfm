86171e4e7f74d0b66548d125b98551eb781eadc2
Lorenzo Bolognese <57444324+lorenzobolognese@users.noreply.github.com>
2019-12-12 10:26:02 +0100

    Manage missing path exception

--- ui.py
+++ ui.py
@@ -13,15 +13,28 @@ class Print(object):
     def __init__(self, dumpToStdOutput = True, dumpToFile = False, fileName = ""):
         self.dumpToStdOutput = dumpToStdOutput
         self.dumpToFile = dumpToFile
-        if self.dumpToFile == True:
-            self.f = open(fileName, "w+")
+        if self.dumpToFile == True: self.__openTxt(fileName)
+
+    def __openTxt(self, fileName):
+        try: self.f = open(fileName, "w+")
+        except Exception as e:
+            self.dumpToFile = False
+            print(e)
+
+    def __writeTxt(self, message):
+        try: self.f.write(str(message) + "\n")
+        except Exception as e: print(e)
+
+    def __closeTxt(self):
+        try: self.f.close()
+        except Exception as e: print(e)
 
     def Out(self, message):
         if self.dumpToStdOutput == True: print(message)
-        if self.dumpToFile == True: self.f.write(str(message) + "\n")
+        if self.dumpToFile == True: self.__writeTxt(message)
 
     def Close(self):
-        if self.dumpToFile == True: self.f.close()
+        if self.dumpToFile == True: self.__closeTxt()
 
 if __name__ == '__main__':
-    main()
+    pass
